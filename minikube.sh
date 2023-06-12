a8242a072a594843299a2adc1c1ea9917cc30050
Wer der Sonne entgegengeht, hat den Schatten hinter sich <henrik.seiffert@gmail.com>
2023-06-12 17:10:45 +0200

    Create minikube.sh

---
+++ minikube.sh
@@ -0,0 +1,6 @@
+#https://www.howtoforge.de/anleitung/so-installierst-du-podman-als-docker-alternative-unter-debian-11/
+apt-get install curl vim podman -y
+
+#https://minikube.sigs.k8s.io/docs/start/
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+install minikube-linux-amd64 /usr/local/bin/minikube
