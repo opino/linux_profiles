c613759b9812c191e44dca96e6964d349559f46b
Wer der Sonne entgegengeht, hat den Schatten hinter sich <henrik.seiffert@gmail.com>
2023-06-12 17:13:13 +0200

    Update minikube.sh

--- minikube.sh
+++ minikube.sh
@@ -4,3 +4,8 @@ apt-get install curl vim podman -y
 #https://minikube.sigs.k8s.io/docs/start/
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 install minikube-linux-amd64 /usr/local/bin/minikube
+
+## Minikube starten
+#https://minikube.sigs.k8s.io/docs/drivers/podman/
+minikube config set rootless true
+minikube start --driver=podman --container-runtime=containerd
